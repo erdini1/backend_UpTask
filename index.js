fb25f702469f37b2a4597f3dc9d24105261d46e4
Dylan Erdini <erdini.dylan@gmail.com>
2023-07-03 13:08:21 -0300

    Creando la conexión a MongoDB

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from "express"
+import dotenv from "dotenv"
+import conectarDB from "./config/db.js"
 
 const app = express()
 
-console.log("Desde Index.js")
+dotenv.config()
 
-app.listen(4000, () => {
-    console.log("Servidor corriendo en puerto 4000")
+conectarDB()
+
+// Esto es para que utilice el puerto que se le asigne cuando se suba pero si no hay como es el caso de desarrollo se utiliza el puerto 4000
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`)
 })
