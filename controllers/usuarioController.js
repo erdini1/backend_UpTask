5c8b684ffd1df1194a6dbd5f03ad384e92f176e7
Dylan Erdini <erdini.dylan@gmail.com>
2023-07-05 13:30:47 -0300

    Evitando registros duplicados

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,15 +1,24 @@
 import Usuario from "../models/Usuario.js"
 
 const registrar = async (req, res) => {
+    //Evitar registros duplicados
+    const { email } = req.body
+    const existeUsuario = await Usuario.findOne({ email })
 
-    try {
-        const usuario = new Usuario(req.body)
-        const usuarioAlmacenado = await usuario.save()
-        res.json(usuarioAlmacenado)
-    } catch (error) {
-        console.log(error)
+
+    
+    if (!existeUsuario) {
+        try {
+            const usuario = new Usuario(req.body)
+            const usuarioAlmacenado = await usuario.save()
+            res.json(usuarioAlmacenado)
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        const error = new Error("Usuario ya registrado")
+        return res.status(400).json({msg: error.message})
     }
-
 
 }
 
